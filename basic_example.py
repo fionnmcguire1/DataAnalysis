e3d05a0204f92f28f0b662b92d40990dab1938ee
Fionn Mcguire <fionnmcguire1@gmail.com>
2020-02-02 13:56:21 +0000

    Writing valid json

--- basic_example.py
+++ basic_example.py
@@ -24,13 +24,15 @@ list_of_json_tweets = []
 for tweet in tweets:
         tweet_time = tweet.timestamp
         tweet.timestamp = tweet_time.strftime('%Y-%m-%d %H:%M:%S')
-
         list_of_json_tweets.append(vars(tweet))
-        print(tweet.text)
 
 #df = pd.DataFrame(t._dict_ for t in tweets)
 str_date = begin_date.strftime("%Y-%m-%d")
 jsonfile = open(data_folder+str_date+".json","w")
-for item in list_of_json_tweets:
+length_of_tweet_list = len(list_of_json_tweets)
+for index,item in enumerate(list_of_json_tweets):
     json.dump(item,jsonfile)
+    if length_of_tweet_list-1 != index:
+        jsonfile.write(",")
+
 jsonfile.close()
