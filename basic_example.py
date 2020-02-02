ad96c8ba7ed4e0779976ff4d48b2341802bee3ed
Fionn Mcguire <fionnmcguire1@gmail.com>
2020-02-02 13:50:10 +0000

    Writing data to json file

--- basic_example.py
+++ basic_example.py
@@ -4,15 +4,33 @@
 from twitterscraper import query_tweets
 import datetime as dt
 import pandas as pd
+import json
 
 
-begin_date = dt.date(2019,4,15)
-end_date = dt.date(2019,4,18)
+
+begin_date = dt.date.today()
+end_date = begin_date + dt.timedelta(days=1)
+data_folder = "JSONDataFiles/"
 
 
-limit = 5
+limit = 1000
 lang = 'english'
 
 
-tweets = query_tweets('notre dame fire', begindate = begin_date, enddate = end_date, limit = limit, lang = lang)
-df = pd.DataFrame(t._dict_ for t in tweets)
+tweets = query_tweets('#sixnations', begindate = begin_date, enddate = end_date, limit = limit, lang = lang)
+
+#print(tweets[0].keys())
+list_of_json_tweets = []
+for tweet in tweets:
+        tweet_time = tweet.timestamp
+        tweet.timestamp = tweet_time.strftime('%Y-%m-%d %H:%M:%S')
+
+        list_of_json_tweets.append(vars(tweet))
+        print(tweet.text)
+
+#df = pd.DataFrame(t._dict_ for t in tweets)
+str_date = begin_date.strftime("%Y-%m-%d")
+jsonfile = open(data_folder+str_date+".json","w")
+for item in list_of_json_tweets:
+    json.dump(item,jsonfile)
+jsonfile.close()
