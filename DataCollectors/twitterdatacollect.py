265ec2bf6274aaebf030dbc6cc76584e99735e3b
Fionn Mcguire <fionnmcguire1@gmail.com>
2020-02-03 00:39:05 +0000

    Adding Testing for build markers

--- DataCollectors/twitterdatacollect.py
+++ DataCollectors/twitterdatacollect.py
@@ -109,31 +109,3 @@ class TwitterDataCollect(object):
                 json_file.write(",")
 
         json_file.close()
-
-
-#Test calls
-test_collector = TwitterDataCollect()
-
-#Testing Build Markers method
-#Correct Case
-response = test_collector.buildMarkers(["#6nations","sixnations","france"])
-print(test_collector.markers)
-print(response)
-
-#Passing markers string instead of list
-test_collector = TwitterDataCollect()
-response = test_collector.buildMarkers("6 nations")
-print(test_collector.markers)
-print(response)
-
-#Passing markers int instead of list
-test_collector = TwitterDataCollect()
-response = test_collector.buildMarkers(5)
-print(test_collector.markers)
-print(response)
-
-#Passing markers empty parameters
-test_collector = TwitterDataCollect()
-response = test_collector.buildMarkers()
-print(test_collector.markers)
-print(response)
