0bf39b32d3275f5bf5dd40d348f73ebceb0841b9
Fionn Mcguire <fionnmcguire1@gmail.com>
2020-02-23 00:46:09 +0000

    Running basic flask project

--- app/app.py
+++ app/app.py
@@ -1,34 +1,5 @@
-import DataView
-from flask import Flask, request,render_template,url_for
-from flask_sqlalchemy import SQLAlchemy
-from app.models import User
+from flask import Flask
 
 app = Flask(__name__)
-app.config["DEBUG"] = True
-# app.config['SQLAlCHEMY_DATABASE_URI'] = 'sqlite:///dataanalysis.db'
-db = SQLAlchemy(app)
 
-@app.route('/', methods=['GET','POST'])
-def index():
-    if request.method == 'POST':
-        username = request.form['username']
-        email = request.form['email']
-        password = request.form['password']
-
-        new_user = User(username=username,email=email,password=password)
-
-        try:
-            db.session.add(new_user)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue adding your task'
-    else:
-        users = User.query.order_by(User.date_created).all()
-        return render_template('index.html', users=users)
-    return render_template('index.html')
-
-
-if __name__ == "__main__":
-    app.run()
-    app.config['debug'] == True
+from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,31 @@
-import DataView
-from flask import Flask
+from flask import Flask,request,render_template
+from app.models import User
+from app import db
+#app = Flask(__name__)
 
+from app import app
 
-app = Flask(__name__)
-app.config["DEBUG"] = True
-
-
-@app.route('/', methods=['GET','POST'])
+@app.route('/')
 @app.route('/index')
 def index():
-    return "<h3>Hello, World!</h3>"
+    return "Hello, World!"
+
+@app.route("/home", methods=['GET'])
+def home():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+
+        new_user = User(username=username,email=email,password=password)
+
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+            return 'Damn'
+        except:
+            return 'There was an issue adding your task'
+    else:
+        users = User.query.order_by(User.username).all()
+        return render_template('index.html', users=users)
+    return render_template('index.html')
